915f6d285377befead834d4d3c7ab960198cf79c
Bhuuba <sparagovskiu@gmail.com>
2025-05-31 11:35:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Futer from "./components/Futer";
+import Havbar from "./components/Havbar";
+import Hero from "./components/Hero";
+import Servise from "./components/Servise";
+import Tours from "./components/Tours";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Havbar />
+      <Hero />
+      <About />
+      <Servise />
+      <Tours />
+      <Futer />
+    </>
   );
 }
 
