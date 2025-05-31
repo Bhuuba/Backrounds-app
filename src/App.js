d66120d86bbca231e0a8db759b88810d5a64a949
Bhuuba <sparagovskiu@gmail.com>
2025-05-31 15:35:59 +0300

    finish land

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
+import React from "react";
 import About from "./components/About";
-import Futer from "./components/Futer";
-import Havbar from "./components/Havbar";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Servise from "./components/Servise";
+import Navbar from "./components/Navbar";
+import Services from "./components/Services";
 import Tours from "./components/Tours";
 
 function App() {
   return (
-    <>
-      <Havbar />
+    <React.Fragment>
+      <Navbar />
       <Hero />
       <About />
-      <Servise />
+      <Services />
       <Tours />
-      <Futer />
-    </>
+      <Footer />
+    </React.Fragment>
   );
 }
 
